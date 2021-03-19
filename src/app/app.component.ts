626262af30caa0dd30db9454a3b5aab874dbeafa
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-03-18 21:04:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'application-labIv';
+  firstAge = 0;
+  secondAge = 0;
+  addition = 0;
+
+  calculateAddition() {
+    this.addition = this.firstAge + this.secondAge;
+    return this.addition;
+  }
+
+  calculateAverage() {
+    return this.addition / (this.addition === this.firstAge || this.addition === this.secondAge ? 1 : 2);
+  }
+
+  clean() {
+    this.firstAge = 0;
+    this.secondAge = 0;
+    this.addition = 0;
+  }
 }
